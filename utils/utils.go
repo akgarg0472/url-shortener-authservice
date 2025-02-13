a32636f3019dc43bd6d38ac626694d505fb1f979
akgarg0472 <akgarg0472@gmail.com>
2025-02-13 22:13:41 +0530

    fix: trailing / in URL endpoints

--- utils/utils.go
+++ utils/utils.go
@@ -1,15 +1,17 @@
 package utils
 
+import "strings"
+
 func GenerateForgotPasswordTokenRedirectUrl(email string, token string) string {
 	frontendBaseUrl := GetEnvVariable("FRONTEND_BASE_DOMAIN", "http://127.0.0.1:3000/")
 	resetPasswordPageUrl := GetEnvVariable("FRONTEND_RESET_PASSWORD_PAGE_URL", "reset-password")
-	return frontendBaseUrl + resetPasswordPageUrl + "?token=" + token + "&email=" + email
+	return EnsureTrailingSlash(frontendBaseUrl) + resetPasswordPageUrl + "?token=" + token + "&email=" + email
 }
 
 func GenerateForgotPasswordLink(email string, forgotPasswordToken string) string {
 	backendBaseUrl := GetEnvVariable("BACKEND_BASE_DOMAIN", "http://localhost:8765/")
 	backendResetPasswordUrl := GetEnvVariable("BACKEND_RESET_PASSWORD_URL", "auth/v1/reset-password")
-	return backendBaseUrl + backendResetPasswordUrl + "?email=" + email + "&token=" + forgotPasswordToken
+	return EnsureTrailingSlash(backendBaseUrl) + backendResetPasswordUrl + "?email=" + email + "&token=" + forgotPasswordToken
 }
 
 func GetStringOrNil(s *string) string {
@@ -26,4 +28,11 @@ func GetInt64OrNil(i *int64) int64 {
 	}
 
 	return -1
+}
+
+func EnsureTrailingSlash(url string) string {
+	if !strings.HasSuffix(url, "/") {
+		return url + "/"
+	}
+	return url
 }
