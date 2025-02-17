e4d6957f88cb2527235df1279fdd9d1538f410db
akgarg0472 <akgarg0472@gmail.com>
2025-02-17 22:42:05 +0530

    updated eureka to consul service discovery

--- discovery-client/discoveryClient.go
+++ discovery-client/discoveryClient.go
@@ -48,11 +48,6 @@ func registerService(port int, host string) {
 		Name:    serviceName,
 		Port:    port,
 		Address: host,
-		Check: &api.AgentServiceCheck{
-			HTTP:     fmt.Sprintf("http://%s:%d/admin/health", host, port),
-			Interval: "30s",
-			Timeout:  "5s",
-		},
 	}
 
 	retryDelay := utils.GetEnvDurationSeconds("REGISTER_RETRY_DELAY_SECONDS", 5*time.Second)
