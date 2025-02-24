8952128b8431f61b3e56ba7253a6a685f62f80ba
akgarg0472 <akgarg0472@gmail.com>
2025-02-25 01:18:02 +0530

    fix: profile_picture_url type from VARCHAR(255) to TEXT

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -3,14 +3,14 @@ package entity
 import enums "github.com/akgarg0472/urlshortener-auth-service/constants"
 
 type User struct {
-	Id                    string                    `gorm:"primaryKey;size:128" json:"id"`                 // varchar(128)
-	Email                 *string                   `gorm:"uniqueIndex;size:255" json:"email"`             // varchar(255)
-	Password              *string                   `gorm:"size:255;nullable=true" json:"password"`        // varchar(255)
-	Scopes                string                    `gorm:"size:32" json:"scopes"`                         // varchar(32)
-	Name                  string                    `gorm:"size:255" json:"name"`                          // varchar(255)
-	Bio                   *string                   `gorm:"type:text" json:"bio,omitempty"`                // text
-	ProfilePictureURL     *string                   `gorm:"size:255" json:"profile_picture_url,omitempty"` // varchar(255)
-	Phone                 *string                   `gorm:"size:20" json:"phone,omitempty"`                // varchar(20)
+	Id                    string                    `gorm:"primaryKey;size:128" json:"id"`                  // varchar(128)
+	Email                 *string                   `gorm:"uniqueIndex;size:255" json:"email"`              // varchar(255)
+	Password              *string                   `gorm:"size:255;nullable=true" json:"password"`         // varchar(255)
+	Scopes                string                    `gorm:"size:32" json:"scopes"`                          // varchar(32)
+	Name                  string                    `gorm:"size:255" json:"name"`                           // varchar(255)
+	Bio                   *string                   `gorm:"type:text" json:"bio,omitempty"`                 // text
+	ProfilePictureURL     *string                   `gorm:"type:text" json:"profile_picture_url,omitempty"` // text
+	Phone                 *string                   `gorm:"size:20" json:"phone,omitempty"`                 // varchar(20)
 	UserLoginType         enums.UserEntityLoginType `gorm:"type:varchar(50)" json:"login_type"`
 	OAuthId               *string                   `gorm:"column:oauth_id;uniqueIndex;size:255" json:"oauth_id,omitempty"`
 	OAuthProvider         *string                   `gorm:"column:oauth_provider;size:16" json:"oauth_provider,omitempty"`
